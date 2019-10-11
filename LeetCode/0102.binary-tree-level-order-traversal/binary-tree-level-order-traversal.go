3cef251f9783357023fafced015593d1ba55ed3c
yahuian <anyahui741600@outlook.com>
2019-10-11 11:32:22 +0800

    improved: 优化BFS方法; added: 添加DFS方法

--- LeetCode/0102.binary-tree-level-order-traversal/binary-tree-level-order-traversal.go
+++ LeetCode/0102.binary-tree-level-order-traversal/binary-tree-level-order-traversal.go
@@ -8,25 +8,20 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+// 广度优先周游 BFS
 func levelOrder(root *TreeNode) [][]int {
 	if root == nil {
-		return [][]int{}
+		return nil
 	}
 	aQueue := DataStructure.Queue{}
-	result := [][]int{}
-	// 初始化队列
-	level := 0
 	aQueue.Push(root)
-
+	var res [][]int
 	for aQueue.Len() != 0 {
+		var tmp []int
 		length := aQueue.Len()
-		// 每增加一层，二维切片就添加一行
-		result = append(result, []int{})
 		for i := 0; i < length; i++ {
 			node, _ := aQueue.Pop()
-			result[level] = append(result[level], node.(*TreeNode).Val)
-
-			// 将当前节点的左右孩子入队
+			tmp = append(tmp, node.(*TreeNode).Val)
 			if node.(*TreeNode).Left != nil {
 				aQueue.Push(node.(*TreeNode).Left)
 			}
@@ -34,7 +29,28 @@ func levelOrder(root *TreeNode) [][]int {
 				aQueue.Push(node.(*TreeNode).Right)
 			}
 		}
-		level++
+		res = append(res, tmp)
 	}
-	return result
+	return res
+}
+
+// 深度优先周游 DFS
+func deepOrder(root *TreeNode) [][]int {
+	var res [][]int
+	helper(root, 0, &res)
+	return res
+}
+
+// 注意res必须为指针类型，保证每次递归时的操作都时针对一个res
+// 如果传递为[][]int类型，则每次递归时二维切片都会被复制
+func helper(root *TreeNode, level int, res *[][]int) {
+	if root == nil {
+		return
+	}
+	if len(*res) == level {
+		*res = append(*res, []int{})
+	}
+	(*res)[level] = append((*res)[level], root.Val)
+	helper(root.Left, level+1, res)
+	helper(root.Right, level+1, res)
 }
