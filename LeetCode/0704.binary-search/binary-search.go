e51fb4bdac3a5eb3ce374fca0e4bc0c96a769339
yahuian <anyahui741600@outlook.com>
2019-10-15 22:24:44 +0800

    added: 二分查找

---
+++ LeetCode/0704.binary-search/binary-search.go
@@ -0,0 +1,17 @@
+package _0704
+
+func search(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return -1
+}
