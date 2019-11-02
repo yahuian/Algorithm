8b1172b92bd7bed396338ff2e4b3c1dac6108046
yahuian <anyahui741600@outlook.com>
2019-11-02 10:20:31 +0800

    百度电话面试

---
+++ LeetCode/0160.intersection-of-two-linked-lists/intersection-of-two-linked-lists.go
@@ -0,0 +1,27 @@
+package _0160
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	if headA == nil || headB == nil {
+		return nil
+	}
+	pA := headA
+	pB := headB
+	for pA != pB {
+		if pA == nil {
+			pA = headB
+		} else {
+			pA = pA.Next
+		}
+		if pB == nil {
+			pB = headA
+		} else {
+			pB = pB.Next
+		}
+	}
+	return pA
+}
