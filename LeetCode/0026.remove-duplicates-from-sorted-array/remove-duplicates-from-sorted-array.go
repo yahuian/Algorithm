c75b4c3b68cc35b1682cf447777bcc8bedf03b65
yahuian <anyahui741600@outlook.com>
2019-11-08 10:47:15 +0800

    added: 删除排序数组中的重复项

---
+++ LeetCode/0026.remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.go
@@ -0,0 +1,15 @@
+package _0026
+
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	i := 0
+	for j := 1; j < len(nums); j++ {
+		if nums[i] != nums[j] {
+			i++
+			nums[i] = nums[j]
+		}
+	}
+	return i + 1
+}
