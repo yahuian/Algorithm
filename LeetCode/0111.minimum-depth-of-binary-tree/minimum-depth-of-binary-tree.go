2b6cb6435d38d09f96f703c1e6d448f950881db3
yahuian <anyahui741600@outlook.com>
2019-12-24 10:58:55 +0800

    improved: 二叉树-二叉树的最小深度，递归法

--- LeetCode/0111.minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.go
+++ LeetCode/0111.minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.go
@@ -1,7 +1,5 @@
 package _0111
 
-import "Algorithm/DataStructure"
-
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -12,27 +10,20 @@ func minDepth(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
-	aQueue := DataStructure.Queue{}
-	aQueue.Push(root)
-	var res [][]int
-	for aQueue.Len() != 0 {
-		length := aQueue.Len()
-		var tmp []int
-		for i := 0; i < length; i++ {
-			node, _ := aQueue.Pop()
-			tmp = append(tmp, node.(*TreeNode).Val)
-			if node.(*TreeNode).Left != nil {
-				aQueue.Push(node.(*TreeNode).Left)
-			}
-			if node.(*TreeNode).Right != nil {
-				aQueue.Push(node.(*TreeNode).Right)
-			}
-			// 左右孩子都为空
-			if node.(*TreeNode).Left == nil && node.(*TreeNode).Right == nil {
-				return len(res) + 1
-			}
-		}
-		res = append(res, tmp)
+	left := minDepth(root.Left)
+	right := minDepth(root.Right)
+
+	// 叶子节点是指没有子结点的节点
+	if left == 0 {
+		return right + 1
 	}
-	return len(res)
+	if right == 0 {
+		return left + 1
+	}
+
+	if left < right {
+		return left + 1
+	} else {
+		return right + 1
+	}
 }
